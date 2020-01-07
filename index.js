f1a4a885dbf4801660036081fe11b0d04088c81f
Unknown <doprp25@yahoo.com>
2020-01-07 23:33:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = greeting2 + specialGuest + "!";
 
-var topic = "space.";
+var topic = "space";
 
-var conversation = "Let's talk about ${topic}";
+var conversation = "+Let's talk about ${topic}.";
